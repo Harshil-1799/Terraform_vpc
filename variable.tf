16d73ff929c66f3fd2e17bc7d63e1227c0ad38b8
Harshil-1799 <shahharshil1706@gmail.com>
2023-08-24 18:04:36 +0530

    variables.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "region"{
+ default ="ap-south-1"
+}
+variable "ami"{
+ default ="ami-0123456789"
+}
+variable "access_key"{
+ default ="As Per Your Account"
+}
+variable "secret_key"{
+ default ="As per Your Account"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
